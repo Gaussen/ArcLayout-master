4417ec38f58efcdc68143cb584d49d207169c435
wang.cheng <WangCheng@CHEN-PC>
2016-05-11 10:23:17 +0800

    新增类测试——修改

--- demo/src/main/java/com/ogaclejapan/arclayout/demo/widget/GitTest.java
+++ demo/src/main/java/com/ogaclejapan/arclayout/demo/widget/GitTest.java
@@ -1,7 +1,17 @@
 package com.ogaclejapan.arclayout.demo.widget;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 /**
  * Created by wangc on 2016/5/11.
  */
-public class GitTest {
+public class GitTest  extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
